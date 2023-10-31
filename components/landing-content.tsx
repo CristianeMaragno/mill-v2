6840473794f09afe8a6bf2bd5ae01a5e20399e90
Cristiane R. Maragno <cristianerodriguesmaragno@gmail.com>
2023-10-31 20:28:15 -0300

    Init template commit

---
+++ components/landing-content.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const testimonials = [
+  {
+    name: "Joel",
+    avatar: "J",
+    title: "Software Engineer",
+    description: "Essa é a melhor ferramenta que eu já usei!",
+  },
+  {
+    name: "Antonio",
+    avatar: "A",
+    title: "Designer",
+    description: "Eu uso diarimente e facilita muito a minha vida.",
+  },
+  {
+    name: "Mark",
+    avatar: "M",
+    title: "CEO",
+    description: "Esse app mudou minha vida! Não consigo pensar em trabalhar sem ele",
+  },
+  {
+    name: "Mary",
+    avatar: "M",
+    title: "CFO",
+    description: "Muito bom, definitivamente vale a assinatura premium",
+  },
+];
+
+export const LandingContent = () => {
+  return (
+    <div className="px-10 pb-20">
+      <h2 className="text-center text-4xl text-white font-extrabold mb-10">O que nossos cliente dizem</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {testimonials.map((item) => (
+          <Card key={item.description} className="bg-[#192339] border-none text-white">
+            <CardHeader>
+              <CardTitle className="flex items-center gap-x-2">
+                <div>
+                  <p className="text-lg">{item.name}</p>
+                  <p className="text-zinc-400 text-sm">{item.title}</p>
+                </div>
+              </CardTitle>
+              <CardContent className="pt-4 px-0">
+                {item.description}
+              </CardContent>
+            </CardHeader>
+          </Card>
+        ))}
+      </div>
+    </div>
+  )
+}
