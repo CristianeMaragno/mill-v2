43c76dca4f5f7a34e770c8ff8e5b8407bfe20e70
Cristiane R. Maragno <cristianerodriguesmaragno@gmail.com>
2023-11-26 22:43:21 -0300

    Add fix

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,31 +1,7 @@
 import { checkSubscription } from "@/lib/subscription";
-//import { modules } from "@/constants";
+import { modules } from "@/constants";
 import { ModuleCard } from "@/components/module-card";
 import { CandlestickChart, Pencil, PiggyBank } from "lucide-react";
-
-export const modules = [
-  {
-    label: 'Corretoras de investimentos',
-    icon: Pencil,
-    href: '/courses/8929b096-3379-4d6e-a643-8dac5a22b779',
-    description: "Corretoras de investimentos atuam como pontes estratégicas entre investidores e mercados, simplificando o acesso a oportunidades de investimento e proporcionando ferramentas essenciais para a construção de portfólios sólidos.",
-    premium: false
-  },
-  {
-    label: 'Tesouro direto I',
-    icon: PiggyBank,
-    href: '/courses/2177a7fc-14f4-4b4a-a69b-de1305386bd3',
-    description: "O Tesouro Direto, como uma modalidade de investimento acessível e segura, oferece aos investidores a oportunidade de participar no financiamento da dívida pública, combinando praticidade e rendimentos atrativos.",
-    premium: true
-  },
-  {
-    label: 'Tesouro direto II',
-    icon: CandlestickChart,
-    href: '/courses/d30c13d6-875c-429d-a145-06fc40aaeafd',
-    description: "Dominar o conhecimento avançado do Tesouro Direto envolve uma profunda compreensão dos diversos títulos disponíveis, estratégias avançadas de alocação de ativos e a capacidade de otimizar a gestão de risco.",
-    premium: true
-  }
-];
 
 
 const DashboardPage = async () => {
