34e87e2ed53a884367ef450a9af271459334b6ef
Cristiane R. Maragno <cristiane.maragno@effecti.com.br>
2024-06-15 19:57:13 -0300

    Add fix

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -100,12 +100,6 @@ const ChapterIdPage = async ({
             </>
           )}
           <Separator />
-          <CourseQuestion
-            chapterId={params.chapterId}
-            courseId={params.courseId}
-            title={chapter.question}
-          >
-          </CourseQuestion>
         </div>
       </div>
     </div>
